83b8022e1ae177591ff251a1459aaff7e6149d0c
Murali Nutalapati <info@muraliprashanth.me>
2017-07-20 00:27:02 +0530

    Persists to localstorage used redux-persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-
+import { compose, createStore, applyMiddleware } from 'redux';
+import {persistStore, autoRehydrate} from 'redux-persist'
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+    reducers,
+    undefined,
+    compose(
+        applyMiddleware(),
+        autoRehydrate()
+    )
+);
+
+persistStore(store);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
